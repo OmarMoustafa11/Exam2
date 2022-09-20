0a35df72d67d3ca0d14b7d3a9fe73a4b4d22b6ca
omarmoustafa11 <omaroj99@yahoo.com>
2022-09-20 15:56:49 -0400

    exam

---
+++ src/main/java/Exam/Cat.java
@@ -0,0 +1,6 @@
+package Exam;
+
+public abstract class Cat extends Feline implements Pet  {
+    public Cat(String name) {
+    }
+}
---
+++ src/main/java/Exam/Dog.java
@@ -0,0 +1,8 @@
+package Exam;
+
+public abstract class Dog extends Canine implements Pet{
+    public Dog(String name) {
+
+    }
+
+}
